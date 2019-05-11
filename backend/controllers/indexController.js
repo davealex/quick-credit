bd77e74aa6275baa4e6de45a0e316a8562c9130a
David Abiola Olaleye <daveabiola@gmail.com>
2019-05-11 03:07:33 +0100

    chore(test-coverage-badges): add test coverage badges to README
    [Finishes #165950974]

---
+++ backend/controllers/indexController.js
@@ -0,0 +1,13 @@
+exports.getIndex = (req, res) => {
+  res.status(200).json(
+    {
+      status: 200,
+      message: 'Welcome to Quick Credit!',
+      data: {
+        service: 'quick-credit',
+        version: '1.0',
+        github: 'https://github.com/davealex/quick-credit',
+      },
+    },
+  );
+};
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const Users = require('../controllers/userController');
+const Index = require('../controllers/indexController');
 
 const apiVersion = '/api/v1';
 
-router.get(`${apiVersion}/users`, Users.getAllUsers);
+router.get('/', Index.getIndex);
 // user endpoints
 router.post(`${apiVersion}/auth/signup`, Users.signUp);
 
