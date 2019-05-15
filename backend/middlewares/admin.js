f1c495c06164fbdbf60116bea3fbd2fa4f5ee641
David Abiola Olaleye <daveabiola@gmail.com>
2019-05-15 06:31:11 +0100

    feature(user-repaid-loans): create user repaid loans api
    - create test(s) for repaid loans endpoint
    - implement api endpoint to pass test condition(s)
    
    [Finishes #165960651]

--- backend/middlewares/admin.js
+++ backend/middlewares/admin.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.headers.is_admin === 'true') next();
+  if (req.headers.is_admin === true) next();
   return res.status(400).json({
     status: 400,
     error: 'You do not have authorization for this action',
